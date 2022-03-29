fa71280b9c8a0a1bc5cddf05858752e05d6d2fc2
Takuya Matsumoto <tmatsnj@gmail.com>
2022-03-29 13:30:25 -0400

    got the todos task to show up

---
+++ src/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+
+function TodoList(props){
+    return (
+        <Paper>
+            {props.todos.map(todo => (
+                <>
+                    <ListItem>
+                        <ListItemText>{todo.task}</ListItemText>
+                    </ListItem>
+                    <Divider />
+                </>
+            ))}
+        </Paper>
+    )
+}
+
+export default TodoList;
